555987f5b27daff9e9637f5f83876ae44977cf50
Nguyen Khanh <nguyenkhanh17112003@gmail.com>
2023-01-14 22:28:01 +0700

    add meta components

---
+++ frontend/components/Meta.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Head from "next/head";
+
+const Meta = function ({ title, keywords, description, icon }) {
+    retrun(
+        <Head>
+            <title>{title}</title>
+            <meta
+                name="viewport"
+                content="initial-scale=1.0, width=device-width"
+            />
+            <meta name="keywords" content={keywords} />
+            <meta name="decription" content={decription} />
+            <link
+                rel="icon"
+                href="https://shop.cardano2vn.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FAdaCoinSmall.ecf61ff1.png&w=32&q=75"
+            />
+            <meta charSet="UTF-8" />
+        </Head>,
+    );
+};
+Meta.defaultProps = {
+    title: "Cardano Shop",
+    keywords: "Cardano Shop",
+    decription: "Cardano Shop",
+};
+
+export default Meta;
